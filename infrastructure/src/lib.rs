3b0c59bd5d85315d40d752e765287033a818e23d
Dallin Huff <dallinhuff@gmail.com>
2025-02-01 20:54:27 -0700

    feat(infrastructure/vendor): Implement PgVendorRepository
    - create a postgres adapter for VendorRepository using sqxl

--- infrastructure/src/lib.rs
+++ infrastructure/src/lib.rs
@@ -5,3 +5,15 @@
 
 pub mod user;
 pub mod vendor;
+
+pub(crate) enum PgError {
+    Unknown(Box<dyn std::error::Error>),
+}
+
+impl From<sqlx::Error> for PgError {
+    fn from(err: sqlx::Error) -> Self {
+        match err {
+            _ => PgError::Unknown(Box::new(err)),
+        }
+    }
+}
