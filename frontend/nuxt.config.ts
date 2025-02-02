6194d216145d186959cbdbd85daa7dd774448a91
Dallin Huff <dallinhuff@gmail.com>
2025-02-01 18:41:46 -0700

    fix(frontend/deps): Remove unnecessary modules & dependencies
    - remove tailwind nuxt & node modules
    - fix lint script to run npm instead of yarn

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,26 +1,15 @@
 import Aura from "@primevue/themes/aura";
-import TailwindTypography from "@tailwindcss/typography";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/content",
-    "@primevue/nuxt-module",
-    "@nuxtjs/tailwindcss",
-    "@nuxt/eslint",
-  ],
+  modules: ["@nuxt/content", "@primevue/nuxt-module", "@nuxt/eslint"],
   primevue: {
     options: {
       theme: {
         preset: Aura,
       },
-    },
-  },
-  tailwindcss: {
-    config: {
-      plugins: [TailwindTypography],
     },
   },
 });
