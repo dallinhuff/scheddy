f52710ed9af7aa7950a97db321df51b1aa8f8a99
Dallin Huff <dallinhuff@gmail.com>
2025-02-03 01:41:51 -0700

    feat(application): Define OfferingRepository port trait

---
+++ application/src/offering.rs
@@ -0,0 +1,44 @@
+pub mod service {
+    use crate::offering::ports::OfferingRepository;
+
+    pub trait OfferingService {}
+
+    pub struct OfferingServiceLive<T: OfferingRepository> {
+        repo: T,
+    }
+
+    impl<T: OfferingRepository> OfferingServiceLive<T> {
+        pub fn new(repo: T) -> OfferingServiceLive<T> {
+            Self { repo }
+        }
+    }
+
+    impl<T: OfferingRepository> OfferingService for OfferingServiceLive<T> {}
+}
+
+pub mod ports {
+    use domain::offering::{Offering, OfferingId};
+    use domain::vendor::VendorId;
+    use std::future::Future;
+
+    pub trait OfferingRepository {
+        fn get_by_id(
+            &self,
+            id: OfferingId,
+        ) -> impl Future<Output = Result<Option<Offering>, RepositoryError>>;
+
+        fn get_by_vendor(
+            &self,
+            vendor_id: VendorId,
+        ) -> impl Future<Output = Result<Vec<Offering>, RepositoryError>>;
+
+        fn save(
+            &self,
+            offering: Offering,
+        ) -> impl Future<Output = Result<Offering, RepositoryError>>;
+
+        fn delete(&self, id: OfferingId) -> impl Future<Output = Result<(), RepositoryError>>;
+    }
+
+    pub enum RepositoryError {}
+}
